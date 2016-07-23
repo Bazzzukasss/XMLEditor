b29a0a1a60e50c05d479d8983af299f2ef84ac37
Bazzzukasss <callocmallocrealloc@gmail.com>
2016-07-23 11:22:49 +0300

    Simple commit.

--- XMLEditor/XMLEditor/xmlwidget.cpp
+++ XMLEditor/XMLEditor/xmlwidget.cpp
@@ -15,7 +15,7 @@ XMLWidget::XMLWidget(QWidget *parent)
     updateFileLabel("");
     mModel =  new TreeModel(":/res/res/xml_source_format.xml");
     mTreeView->setModel(mModel);
-    mTreeView->expandAll();
+    //mTreeView->expandAll();
     slotViewResize();
 
     mSelectionModel = new QItemSelectionModel(mModel);
@@ -30,6 +30,7 @@ XMLWidget::XMLWidget(QWidget *parent)
 XMLWidget::~XMLWidget()
 {
     delete mModel;
+    //delete mSelectionModel;
 }
 
 void XMLWidget::build()
@@ -49,17 +50,17 @@ void XMLWidget::slotOpen()
     QString filename = QFileDialog::getOpenFileName(this, "Open file", mModel->getFileName(), "files(*.xml )");
     if(!filename.isEmpty())
     {
-        delete mModel;
         //delete mSelectionModel;
+        delete mModel;
 
         mModel = new TreeModel(filename);
-        //mSelectionModel = new QItemSelectionModel(mModel);
+        mSelectionModel = new QItemSelectionModel(mModel);
 
 
         mTreeView->setModel(mModel);
-        //mTreeView->setSelectionModel(mSelectionModel);
+        mTreeView->setSelectionModel(mSelectionModel);
 
-        mTreeView->expandAll();
+        //mTreeView->expandAll();
         updateFileLabel(filename);
     }
 }
@@ -101,44 +102,45 @@ void XMLWidget::slotRevertAll()
 
 void XMLWidget::slotInsertSubTag()
 {
-    QModelIndex index = mTreeView->selectionModel()->currentIndex();
-    QAbstractItemModel *model = mTreeView->model();
+    QModelIndex index = mSelectionModel->currentIndex();
 
-    if (model->columnCount(index) == 0) {
-        if (!model->insertColumn(0, index))
+    if (mModel->columnCount(index) == 0) {
+        if (!mModel->insertColumn(0, index))
             return;
     }
 
-    if (!model->insertRow(0, index))
+    if (!mModel->insertRow(0, index))
         return;
 
-    for (int column = 0; column < model->columnCount(index); ++column) {
-        QModelIndex child = model->index(0, column, index);
-        model->setData(child, QVariant("[No data]"), Qt::EditRole);
+    for (int column = 0; column < mModel->columnCount(index); ++column) {
+        QModelIndex child = mModel->index(0, column, index);
+        mModel->setData(child, QVariant("data"), Qt::EditRole);
         //if (!model->headerData(column, Qt::Horizontal).isValid())
           //  model->setHeaderData(column, Qt::Horizontal, QVariant("[No header]"), Qt::EditRole);
     }
 
-    mTreeView->selectionModel()->setCurrentIndex(model->index(0, 0, index), QItemSelectionModel::ClearAndSelect);
+    mSelectionModel->setCurrentIndex(mModel->index(0, 0, index), QItemSelectionModel::ClearAndSelect);
 }
 
 void XMLWidget::slotInsertTag()
 {
-    QModelIndex index = mTreeView->selectionModel()->currentIndex();
-    QAbstractItemModel *model = mTreeView->model();
+    QModelIndex index = mSelectionModel->currentIndex();
 
-    if (!model->insertRow(index.row()+1, index.parent()))
+    if (!mModel->insertRow(index.row()+1, index.parent()))
         return;
 
-    for (int column = 0; column < model->columnCount(index.parent()); ++column) {
-        QModelIndex child = model->index(index.row()+1, column, index.parent());
-        model->setData(child, QVariant("[No data]"), Qt::EditRole);
+    for (int column = 0; column < mModel->columnCount(index.parent()); ++column) {
+        QModelIndex child = mModel->index(index.row()+1, column, index.parent());
+        mModel->setData(child, QVariant("[No data]"), Qt::EditRole);
     }
+
 }
 void XMLWidget::slotDelete()
 {
     QModelIndex index = mSelectionModel->currentIndex();
-    mModel->removeRow(index.row(), index.parent());
+
+    if(index.isValid())
+        mModel->removeRow(index.row(), index.parent());
 }
 void XMLWidget::updateFileLabel(const QString& filename)
 {
